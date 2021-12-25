5ffb2cf6448a41a43d3cafcd6c2df9c6c5d654d0
Vamsi1810 <krishna18vamsi@gmail.com>
2021-12-25 15:40:47 +0530

    25Dec-2

--- src/main/java/com/klef/demo/Finance.java
+++ src/main/java/com/klef/demo/Finance.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class Finance {
 	
 	@Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
 	@Column(name="Email")
